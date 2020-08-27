3ef2018d9ee353656f8677456d2689b7b973a36e
Сухарева Мария <maria_suxareva@mail.ru>
2020-08-27 14:22:43 +0300

    init

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'cache-only-v1';
+// При установке воркера мы должны закешировать часть данных (статику)
+self.addEventListener('install', (event) => {
+    console.log('sw Установлен')
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            return cache.addAll([
+                './sw-test/mus1.mp3'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('sw Активирован');
+});
+
+// При запросе на сервер (событие fetch), используем только данные из кэша.
+self.addEventListener('fetch', (event) => {
+    console.log('sw fetch', event.request);
+    event.respondWith(fromCache(event.request))
+});
+
+function fromCache(request) {
+    console.log('take file from caches');
+    return caches.open(CACHE)
+        .then((cache) => {
+            console.log(cache, 'cache')
+            cache.match(request)
+        })
+        .then((matching) => {
+            console.log(matching, 'matching')
+            matching || Promise.reject('no-match')
+        })
+}
