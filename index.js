a442b44cff6126fe9950add281474af83e08640c
Andrey Dyachenko <andrey.dyachenko@zfx.com>
2020-08-28 17:31:18 +0300

    Some code fixes
    Fixed syntax error in index.js file. Bring HTML in accordance with W3C HTML rules. General code formatting imroves.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Проверка того, что наш браузер поддерживает Service Worker API.
 if ('serviceWorker' in navigator) {
-    console.log("SW in browser")
-    // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('/sw-test/sw.js')
-        .then((reg) => {
-            // регистрация сработала
-            console.log('Registration succeeded. Scope is ' + reg.scope)
-            // navigator.serviceWorker.ready
-        })
-        // .then((worker) => {
-        //     console.log('syncdata')
-        //     worker.sync.register('syncdata')
-        // })
-        .catch((err) => console.log('Registration failed with ' + error));
+	console.log("SW in browser")
+	// Весь код регистрации у нас асинхронный.
+	navigator.serviceWorker.register('/sw-test/sw.js')
+		.then(reg => {
+			// регистрация сработала
+			console.log('Registration succeeded. Scope is ' + reg.scope)
+			// navigator.serviceWorker.ready
+		})
+		// .then((worker) => {
+		// 	console.log('syncdata')
+		// 	worker.sync.register('syncdata')
+		// })
+		.catch(error => console.log('Registration failed with ' + error));
 }
